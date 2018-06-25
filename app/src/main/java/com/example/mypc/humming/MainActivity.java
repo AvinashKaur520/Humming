c53378c8c89fd30fa655c585ba48983ecd286541
AvinashKaur <avinashkaur520@gmail.com>
2018-06-25 17:09:46 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mypc/humming/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.mypc.humming;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "onCreate is running....", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart()
+      {
+          super.onStart();
+          Toast.makeText(this, "onStart is running....", Toast.LENGTH_SHORT).show();
+      }
+    @Override
+      protected void onResume()
+      {
+          super.onResume();
+          Toast.makeText(this, "onResume is running....", Toast.LENGTH_SHORT).show();
+      }
+    @Override
+      protected void onPause()
+      {
+          super.onPause();
+          Toast.makeText(this, "onPause is running....", Toast.LENGTH_SHORT).show();
+      }
+    @Override
+      protected void onRestart()
+      {
+          super.onRestart();
+          Toast.makeText(this, "onRestart is running....", Toast.LENGTH_SHORT).show();
+      }
+    @Override
+      protected void onStop()
+      {
+          super.onStop();
+          Toast.makeText(this, "onStop is running....", Toast.LENGTH_SHORT).show();
+      }
+    @Override
+      protected void onDestroy()
+      {
+          super.onDestroy();
+          Toast.makeText(this, "onDestroy is running....", Toast.LENGTH_SHORT).show();
+      }
+
+    }
